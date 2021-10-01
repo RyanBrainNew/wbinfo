8a2747d567ef91131254b9cba75736fa7037b8ad
LJL <853138590@qq.com>
2021-10-01 13:12:18 +0800

    分离推送模块

---
+++ push.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Author    : B1ain
+# Action    : 微博
+# Desc      : 微博主模块
+
+import requests
+import json
+import re
+
+# 企业微信配置区
+corpid = 'wwb83672685478bbbe'
+AgentId = '1000002'
+corpsecret = 'Frah3vmHcAaSbGWdPYJOgmVALwgNAzWYvx7KbqI7YkM'
+# AgentId = '1000003'
+# corpsecret = 'GVO52NYeeQdgrSpd2Wikje1QNyBRoZzZyexY9k5dFqQ'
+# AgentId = '1000004'
+# corpsecret = 'svupCDwX54V1NtuekwuOg58yW5khHyCnzrTLJy8Vpos'
+
+# telegram机器人
+tg_bot_token = "2023326569:AAHLs6ulU2-NnYXJRnHgx-9_I74AplihwwA"
+tg_user_id = "-520508478"
+
+
+class pushChannels:
+	# 电报机器人推送
+	def telegram_Push(content):
+		if not tg_bot_token or not tg_user_id:
+			print("Telegram推送的tg_bot_token或者tg_user_id未设置!!\n取消推送")
+			return
+		send_data = {"chat_id": tg_user_id, "text": '\n' + str(content), "disable_web_page_preview": "true"}
+		response = requests.post(url='https://api.telegram.org/bot%s/sendMessage' % (tg_bot_token), data=send_data, timeout=5)
+		if response.status_code != 200:
+			print(f"访问失败\n")
+			return
+		respon = response.json()  # 当返回的数据是json串的时候直接用.json即可将respone转换成字典
+		if respon['ok'] == True:
+			print(f"推送成功\n")
+		else:
+			print(f"推送失败:鬼知道哪错了\n")
+
+	# 处理电报推送的文本,去除html格式
+	def telegram_HandleMessage(dicts):
+		context = dicts['text'].re1place('<br />', '\r')
+		pattern = re.compile(r'<[^>]+>', re.S)
+		result = pattern.sub('', context)
+		text = "发送内容: " + result + "\n"
+		text += "发送时间: " + dicts['created_at'][0:20]+"\n"
+		text += "宁关注的：" + dicts['nickName']+"发布微博啦\n"
+		return text
+
+	# 企业微信推送
+	def wechat_Push(msg):
+		server_url = f"https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid={corpid}&corpsecret={corpsecret}"
+		response = requests.post(server_url)
+		jsontxt = json.loads(response.text)
+		access_token = jsontxt['access_token']
+		html = msg.replace('\n', '<br>')
+		url = f"https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token={access_token}"
+		data = \
+			{
+				"touser": "@all",
+				"msgtype": "text",
+				"agentid": AgentId,
+				"text": {
+					"content": html
+				},
+				"safe": 0
+			}
+		send_msges = (bytes(json.dumps(data), 'utf-8'))
+		res = requests.post(url, send_msges)
+		respon = res.json()  # 当返回的数据是json串的时候直接用.json即可将respone转换成字典
+		if respon['errmsg'] == "ok":
+			print(f"推送成功\n")
+		else:
+			print(f" 推送失败:鬼知道哪错了\n")
+
+	# 处理微信推送的文本,去除html格式
+	def wechat_HandleMessage(dicts):
+		context = dicts['text'].replace('<br />', '\r')
+		pattern = re.compile(r'<[^>]+>', re.S)
+		result = pattern.sub('', context)
+		text = "发送内容: " + result + "\r"
+		text += "发送时间: " + dicts['created_at'][0:20]+"\r"
+		text += "宁关注的：" + dicts['nickName']+"发布微博啦\r"
+		return text
--- weibo.py
+++ weibo.py
@@ -5,28 +5,11 @@
 # Desc      : 监控启动模块
 
 import wbmonitor
-import requests
-# import urllib.parse, json
+import push
 import logging
 import time
-import re
 
-# 配置区
-keywords = {"婴", "儿童", "尿"}
-
-# 企业微信配置区
-# corpid = 'wwb83672685478bbbe'
-# AgentId = '1000002'
-# corpsecret = 'Frah3vmHcAaSbGWdPYJOgmVALwgNAzWYvx7KbqI7YkM'
-# AgentId = '1000003'
-# corpsecret = 'GVO52NYeeQdgrSpd2Wikje1QNyBRoZzZyexY9k5dFqQ'
-# AgentId = '1000004'
-# corpsecret = 'svupCDwX54V1NtuekwuOg58yW5khHyCnzrTLJy8Vpos'
-
-# telegram机器人
-tg_bot_token = "2023326569:AAHLs6ulU2-NnYXJRnHgx-9_I74AplihwwA"
-tg_user_id = "-520508478"
-
+# 输出到日志文件
 # logfile_path = '/root/weibo/new.log'
 logfile_path = 'new.log'
 
@@ -38,99 +21,18 @@ logging.basicConfig(level=logging.INFO,  # 控制台打印的日志级别
                     # '%(asctime)s - %(pathname)s[line:%(lineno)d] - %(levelname)s: %(message)s'
                     # 日志格式
                     )
-# 企业微信推送
-# def qywx(msg):
-#     server_url = f"https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid={corpid}&corpsecret={corpsecret}"
-#     re = requests.post(server_url)
-#     jsontxt = json.loads(re.text)
-#     access_token = jsontxt['access_token']
-#     html = msg.replace('\n', '<br>')
-#     url = f"https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token={access_token}"
-#     data = \
-#         {
-#             "touser": "@all",
-#             "msgtype": "text",
-#             #"agentid": "1000002",
-#             "agentid": AgentId,
-#             "text": {
-#                 "content": html
-#             },
-#             "safe": 0
-#         }
-#     send_msges = (bytes(json.dumps(data), 'utf-8'))
-#     res = requests.post(url, send_msges)
-#     respon = res.json()  # 当返回的数据是json串的时候直接用.json即可将respone转换成字典
-#     print(respon)
-#     print(res.text)
-#     if respon['errmsg'] == "ok":
-#         print(f"推送成功\n")
-#     else:
-#         print(f" 推送失败:鬼知道哪错了\n")
-#     print("end")
-
-# telegram推送
-# def telegram_bot(title, content):
-#     print("\n")
-#     tg_bot_token = TG_BOT_TOKEN
-#     tg_user_id = TG_USER_ID
-#     if "TG_BOT_TOKEN" in os.environ and "TG_USER_ID" in os.environ:
-#         tg_bot_token = os.environ["TG_BOT_TOKEN"]
-#         tg_user_id = os.environ["TG_USER_ID"]
-#     if not tg_bot_token or not tg_user_id:
-#         print("Telegram推送的tg_bot_token或者tg_user_id未设置!!\n取消推送")
-#         return
-#     print("Telegram 推送开始")
-#     send_data = {"chat_id": tg_user_id, "text": title +
-#                                                 '\n\n' + content, "disable_web_page_preview": "true"}
-#     response = requests.post(
-#         url='https://api.telegram.org/bot%s/sendMessage' % (tg_bot_token), data=send_data)
-#     print(response.text)
-#
-#
-# now = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-# headers = {
-#     'User-Agent': 'Dalvik/2.1.0 (Linux; U; Android 9; MI 6 MIUI/20.6.18)'
-# }
 
 
-def telegram_bot(title, content):
-    print("\n")
-    if not tg_bot_token or not tg_user_id:
-        print("Telegram推送的tg_bot_token或者tg_user_id未设置!!\n取消推送")
-        return
-    print("Telegram 推送开始")
-    send_data = {"chat_id": tg_user_id, "text": title + '\n' + content, "disable_web_page_preview": "true"}
-    response = requests.post(
-        url='https://api.telegram.org/bot%s/sendMessage' % (tg_bot_token), data=send_data)
-    print(response.text)
+# 关键词过滤
+keywords = {"婴", "儿童", "尿"}
 
 
 def keyword_compare(dicts):
     # print(dicts['text'])
     for i in keywords:
-        if(i in dicts['text']):
+        if i in dicts['text']:
             return 0
     return 1
-
-
-# def wbweixin(dicts):
-#     context = dicts['text'].replace('<br />', '\r')
-#     pattern = re.compile(r'<[^>]+>', re.S)
-#     result = pattern.sub('', context)
-#     text = "发送内容: "  + result + "\r"
-#     text += "发送时间: " + dicts['created_at'][0:20]+"\r"
-#     text += "宁关注的：" + dicts['nickName']+"发布微博啦\r"
-#     flag = qywx(text)
-#     return flag
-
-def handlemessage(dicts):
-    context = dicts['text'].replace('<br />', '\r')
-    pattern = re.compile(r'<[^>]+>', re.S)
-    result = pattern.sub('', context)
-    text = "发送内容: " + result + "\n"
-    text += "发送时间: " + dicts['created_at'][0:20]+"\n"
-    text += "宁关注的：" + dicts['nickName']+"发布微博啦\n"
-    return text
 
 
 def main():
@@ -144,11 +46,15 @@ def main():
     new_wb = w.startmonitor()
     if new_wb is not None:
         flag = keyword_compare(new_wb)
-        if(flag == 1):
+        if flag == 1:
             logging.info('准备推送')
-            texts = handlemessage(new_wb)
-            telegram_bot("微博更新", texts)  # 用telegram机器人推送
-            # print(wbweixin(new_wb))  # 用企业微信推送
+            if 1:
+                texts = push.pushChannels.telegram_HandleMessage(new_wb)
+                push.pushChannels.telegram_Push(texts)  # 用telegram机器人推送
+            # if 1:
+            #     texts = push.pushChannels.wechat_HandleMessage(new_wb)
+            #     push.pushChannels.wechat_Push(texts)  # 用企业微信推送
+
         else:
             print("没有关键词")
     else:
@@ -157,6 +63,6 @@ def main():
 
 
 if __name__ == '__main__':
-    while(1):
+    while 1:
         main()
         time.sleep(30)
